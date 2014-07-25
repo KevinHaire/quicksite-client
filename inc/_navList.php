990f4ebcda734c592c2c195094866ff062af54d0
Kevin Haire <khaire@shopcity.com>
2014-07-25 11:08:15 -0400

    first stable version of one source quicksite

--- inc/_navList.php
+++ inc/_navList.php
@@ -1,6 +1,57 @@
 <ul>
-  <li><a href="">Megan</a></li>
-  <li><a href="">Rob</a></li>
-  <li><a href="">Kevin</a></li>
-  <li><a href="">Corey</a></li>
+  <li>
+      <a href="index.php" target="_self">
+      	Home
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=photos&pageType=gallery" target="_self">
+      	Photos
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=events&pageType=events" target="_self">
+      	Events
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=newsletter&pageType=newsletter" target="_self">
+      	Newsletter
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=posts&pageType=posts" target="_self">
+      	Posts
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=contact&pageType=contact" target="_self">
+      	Contact
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=cart&pageType=store" target="_self">
+      	Cart
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=gift%20certificates&pageType=gift" target="_self">
+      	GC
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=coupons&pageType=coupons" target="_self">
+      	Coupons
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=reviews&pageType=reviews" target="_self">
+      	Reviews
+      </a>
+    </li>
+  <li>
+      <a href="page.php?page=blog&pageType=blog" target="_self">
+      	Blog
+      </a>
+    </li>
 </ul>
